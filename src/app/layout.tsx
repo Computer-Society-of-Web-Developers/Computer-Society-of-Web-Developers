d708254277aca8a779fcc0238c863235ce6416d8
tejus3131 <tejus3131@gmail.com>
2024-04-01 22:03:48 +0530

    added basic structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CSWD",
+  description: "Computer Socity of Web Developers",
+  manifest: "/manifest.json",
+  icons: { apple: "/icon.png" }
+};
+
+export const viewport: Viewport = {
+  themeColor: "#000000",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+  return (
+    <div>welcome to cswd</div>
+  )
+}
